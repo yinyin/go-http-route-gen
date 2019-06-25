89722ac5e1d71841e09e777b1cb3193a7af90c0e
Yinyin L <yinyinl@gmail.com>
2019-06-25 19:00:32 +0800

    verify route configuration

--- httproutegen/parse.go
+++ httproutegen/parse.go
@@ -1,7 +1,9 @@
 package httproutegen
 
 import (
+	"errors"
 	"io/ioutil"
+	"strings"
 
 	yaml "gopkg.in/yaml.v2"
 )
@@ -11,7 +13,37 @@ type RouteEntry struct {
 	Component         string        `yaml:"c,omitempty"`
 	HandlerName       string        `yaml:"name,omitempty"`
 	StrictPrefixMatch string        `yaml:"strict-prefix-match,omitempty"`
+	StrictMatch       bool          `yaml:"strict-match,omitempty"`
 	Routes            []*RouteEntry `yaml:"route,omitempty"`
+}
+
+func (entry *RouteEntry) cleanupComponent(parentComponents string) error {
+	entry.Component = strings.Trim(entry.Component, "/")
+	if "" == entry.Component {
+		return errors.New("empty component: parent=[" + parentComponents + "], handler=[" + entry.HandlerName + "]")
+	}
+	return nil
+}
+
+func (entry *RouteEntry) verifyConfiguration(parentComponents string) error {
+	if err := entry.cleanupComponent(parentComponents); nil != err {
+		return err
+	}
+	componentIdent := parentComponents + entry.Component
+	if ("" != entry.StrictPrefixMatch) && entry.StrictMatch {
+		return &ErrConflictConfiguration{
+			Component: componentIdent,
+			Config1:   "strict-prefix-match=" + entry.StrictPrefixMatch,
+			Config2:   "strict-match=true",
+			Message:   "partial-strict-match and fully-strict-match cannot co-exist",
+		}
+	}
+	for _, childEntry := range entry.Routes {
+		if err := childEntry.verifyConfiguration(componentIdent + "/"); nil != err {
+			return err
+		}
+	}
+	return nil
 }
 
 // LoadYAML get route configuration from YAML file
@@ -24,6 +56,8 @@ func LoadYAML(configFilePath string) (routeEntry *RouteEntry, err error) {
 	if err = yaml.Unmarshal(buf, &routeEntryBuf); nil != err {
 		return
 	}
-	// TODO: post processing
+	if err = routeEntryBuf.verifyConfiguration("/"); nil != err {
+		return
+	}
 	return &routeEntryBuf, nil
 }
