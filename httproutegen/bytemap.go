f8663016bf1b155047f069096fefce72615c898c
Yinyin L <yinyinl@gmail.com>
2019-06-26 15:41:12 +0800

    able to disable bytes

--- httproutegen/bytemap.go
+++ httproutegen/bytemap.go
@@ -38,3 +38,30 @@ func (m *byteMap) enableByteRange(b0, b1 byte) {
 func (m *byteMap) enablePrintables() {
 	m.enableByteRange(0x20, 0x7E)
 }
+
+func (m *byteMap) disableByte(b byte) {
+	if b > 127 {
+		log.Printf("WARN: register byte > 127: %v", b)
+		return
+	}
+	bVal := uint(b)
+	bIndex := 0
+	if 0 != (bVal & 64) {
+		bIndex = 1
+	}
+	offset := bVal & 63
+	m.bits[bIndex] = m.bits[bIndex] & (^uint64(1 << offset))
+}
+
+func (m *byteMap) disableByteRange(b0, b1 byte) {
+	if (b0 > 127) || (b1 > 127) {
+		log.Printf("WARN: register byte range > 127: %v - %v", b0, b1)
+		return
+	}
+	if b1 < b0 {
+		b0, b1 = b1, b0
+	}
+	for b := b0; b <= b1; b++ {
+		m.disableByte(b)
+	}
+}
