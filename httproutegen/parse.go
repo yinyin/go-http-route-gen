a4be659bac043aaf713f499a47af5f3720a68418
Yinyin L <yinyinl@gmail.com>
2019-06-26 18:47:28 +0800

    adjust parsing logic and support JSON dump

--- httproutegen/parse.go
+++ httproutegen/parse.go
@@ -4,32 +4,46 @@ import (
 	"errors"
 	"io/ioutil"
 	"strings"
+	"unicode"
 
 	yaml "gopkg.in/yaml.v2"
 )
 
 // RouteEntry represent an entry of route
 type RouteEntry struct {
-	Component         string        `yaml:"c,omitempty"`
-	HandlerName       string        `yaml:"name,omitempty"`
-	StrictPrefixMatch string        `yaml:"strict-prefix-match,omitempty"`
-	StrictMatch       bool          `yaml:"strict-match,omitempty"`
-	Routes            []*RouteEntry `yaml:"route,omitempty"`
+	Component         string        `yaml:"c,omitempty" json:"c,omitempty"`
+	HandlerName       string        `yaml:"handler,omitempty" json:"handler,omitempty"`
+	StrictPrefixMatch string        `yaml:"strict-prefix-match,omitempty" json:"strict_prefix_match,omitempty"`
+	StrictMatch       bool          `yaml:"strict-match,omitempty" json:"strict_match,omitempty"`
+	TrailingSlash     bool          `yaml:"trailing-slash,omitempty" json:"trailing_slash,omitempty"`
+	Routes            []*RouteEntry `yaml:"route,omitempty" json:"route,omitempty"`
 }
 
-func (entry *RouteEntry) cleanupComponent(parentComponents string) error {
-	entry.Component = strings.Trim(entry.Component, "/")
+func (entry *RouteEntry) makeComponentIdent(parentComponentIdent string) string {
 	if "" == entry.Component {
-		return errors.New("empty component: parent=[" + parentComponents + "], handler=[" + entry.HandlerName + "]")
+		if "" == parentComponentIdent {
+			return "/"
+		}
+		return parentComponentIdent
+	}
+	return parentComponentIdent + entry.Component + "/"
+}
+
+func (entry *RouteEntry) cleanupComponent(parentComponentIdent string) error {
+	entry.Component = strings.TrimFunc(entry.Component, func(ch rune) bool {
+		return unicode.IsSpace(ch) || (ch == '/')
+	})
+	if ("" == entry.Component) && ("" != parentComponentIdent) {
+		return errors.New("empty component: parent=[" + parentComponentIdent + "], handler=[" + entry.HandlerName + "]")
 	}
 	return nil
 }
 
-func (entry *RouteEntry) verifyConfiguration(parentComponents string) error {
-	if err := entry.cleanupComponent(parentComponents); nil != err {
+func (entry *RouteEntry) verifyConfiguration(parentComponentIdent string) error {
+	if err := entry.cleanupComponent(parentComponentIdent); nil != err {
 		return err
 	}
-	componentIdent := parentComponents + entry.Component
+	componentIdent := entry.makeComponentIdent(parentComponentIdent)
 	if ("" != entry.StrictPrefixMatch) && entry.StrictMatch {
 		return &ErrConflictConfiguration{
 			Component: componentIdent,
@@ -38,8 +52,16 @@ func (entry *RouteEntry) verifyConfiguration(parentComponents string) error {
 			Message:   "partial-strict-match and fully-strict-match cannot co-exist",
 		}
 	}
+	if entry.TrailingSlash && ("" == entry.HandlerName) {
+		return &ErrConflictConfiguration{
+			Component: componentIdent,
+			Config1:   "trailing-slash=true",
+			Config2:   "handler=" + entry.HandlerName,
+			Message:   "enabling trailing-slash on terminate component only",
+		}
+	}
 	for _, childEntry := range entry.Routes {
-		if err := childEntry.verifyConfiguration(componentIdent + "/"); nil != err {
+		if err := childEntry.verifyConfiguration(componentIdent); nil != err {
 			return err
 		}
 	}
@@ -56,7 +78,7 @@ func LoadYAML(configFilePath string) (routeEntry *RouteEntry, err error) {
 	if err = yaml.Unmarshal(buf, &routeEntryBuf); nil != err {
 		return
 	}
-	if err = routeEntryBuf.verifyConfiguration("/"); nil != err {
+	if err = routeEntryBuf.verifyConfiguration(""); nil != err {
 		return
 	}
 	return &routeEntryBuf, nil
