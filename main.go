1c16ee31119453c319dba6fe9ab18e1f9689810f
Yinyin L <yinyinl@gmail.com>
2019-06-26 18:48:04 +0800

    dumps loaded configuration for verification

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
+
+	"github.com/yinyin/go-http-route-gen/httproutegen"
 )
 
 func main() {
@@ -12,4 +15,15 @@ func main() {
 	}
 	log.Printf("Input: %v", inputFilePath)
 	log.Printf("Output: %v", outputFilePath)
+	rootRouteEntry, err := httproutegen.LoadYAML(inputFilePath)
+	if nil != err {
+		log.Fatalf("ERR: cannot load route configuration [%s]: %v", inputFilePath, err)
+		return
+	}
+	rootFanoutEntry := httproutegen.NewFanoutEntry(rootRouteEntry)
+	if fanoutJSONText, err := json.MarshalIndent(rootFanoutEntry, "", "  "); nil != err {
+		log.Fatalf("ERR: cannot encode root fanout into JSON: %v", err)
+	} else {
+		log.Print(string(fanoutJSONText))
+	}
 }
